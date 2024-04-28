8b1e9a257c6d813c20e9f740a87b5737e079b4c6
OM RAMANI <123269147+OmRamani98@users.noreply.github.com>
2024-04-28 22:42:49 +0530

    Update videoserver.js

--- videoserver.js
+++ videoserver.js
@@ -45,13 +45,18 @@ app.post('/upload-video', upload.single('videoFile'), async (req, res) => {
     console.log('File uploaded to Google Cloud Storage:', fileName);
 
     // Transcribe the audio from the MP4 file using LongRunningRecognize method
+// const audioConfig = {
+//     sampleRateHertz: 16000,
+//     languageCode: 'en-US',
+//     encoding: 'MP3', // This line specifies the encoding as MP3
+//     enableAutomaticPunctuation: true
+// };
 const audioConfig = {
     sampleRateHertz: 16000,
     languageCode: 'en-US',
-    encoding: 'MP3', // This line specifies the encoding as MP3
+    encoding: 'LINEAR16', // Change encoding to LINEAR16
     enableAutomaticPunctuation: true
 };
-
 
     const audio = {
       uri: `gs://${bucketName}/${fileName}`,
